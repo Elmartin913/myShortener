c12175955bedeef0dd417995d2f1d93300d4de27
Marcin Jablonowski <marcin.jab@wp.pl>
2017-11-22 19:06:57 +0100

    Models Manager and short code generator

--- cutter/models.py
+++ cutter/models.py
@@ -1,13 +1,28 @@
 from django.db import models
 
+from .utils import (code_generator, create_shortcode)
+
 # Create your models here.
+
+
+class ShortURLManager(models.Manager):
+    def all(self, *args, **kwargs):
+        qs = super(ShortURLManager, self).all(*args, **kwargs)
+        qs = qs.filter(active=None)
+        return qs
 
 
 class ShortURL(models.Model):
     url = models.CharField(max_length=256, )
-    shortcode = models.CharField(max_length=15, unique=True)
+    shortcode = models.CharField(max_length=15, unique=True, blank=True, null=True)
     update = models.DateTimeField(auto_now=True)
     timestamp = models.DateTimeField(auto_now_add=True)
+    active = models.BooleanField(default=True)
+
+    def save(self, *args, **kwargs):
+        if self.shortcode is None or self.shortcode == '':
+            self.shortcode = create_shortcode(self)
+        super(ShortURL, self).save(*args, **kwargs)
 
     def __str__(self):
         return str(self.url)
