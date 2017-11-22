7c41eb1a4c7f93ff79ebef843036b52f12f3d5eb
Marcin Jablonowski <marcin.jab@wp.pl>
2017-11-22 21:05:30 +0100

    Models Manager

--- cutter/models.py
+++ cutter/models.py
@@ -11,6 +11,16 @@ class ShortURLManager(models.Manager):
         qs = qs.filter(active=None)
         return qs
 
+    def refresh_shortcode(self):
+        qs = ShortURL.objects.filter(id__gte=1)
+        new_code = 0
+        for q in qs:
+            q.shortcode = create_shortcode(q)
+            print(q.shortcode)
+            q.save()
+            new_code += 1
+        return 'New code made refresh: {}'.format(new_code)
+
 
 class ShortURL(models.Model):
     url = models.CharField(max_length=256, )
@@ -18,6 +28,7 @@ class ShortURL(models.Model):
     update = models.DateTimeField(auto_now=True)
     timestamp = models.DateTimeField(auto_now_add=True)
     active = models.BooleanField(default=True)
+    objects = ShortURLManager()
 
     def save(self, *args, **kwargs):
         if self.shortcode is None or self.shortcode == '':
